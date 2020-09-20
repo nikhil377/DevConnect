d351536379e88147fc2448536a48daf3fbb49890
Nikhil Arora <nikhil.arora@team.telstra.com>
2020-09-21 04:47:53 +0530

    Added routes for the different endpoints and called those endpoints in server.js

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const connectDB = require('./config/db');
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.get('/',(req,res)=>{
-    res.send("API is running ");
-})
+
+// endpoint , path
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
